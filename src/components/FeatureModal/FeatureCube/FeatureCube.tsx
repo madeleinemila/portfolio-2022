4d2a5621bf1c4b5594f6e6eba2beff8f902287e0
madeleinemila <32352098+madeleinemila@users.noreply.github.com>
2022-10-13 15:54:42 +1100

    Add cube modal content

--- src/components/FeatureModal/FeatureCube/FeatureCube.tsx
+++ src/components/FeatureModal/FeatureCube/FeatureCube.tsx
@@ -1,9 +1,47 @@
+import Image from "next/image";
+import cubeImage from "../../../../public/images/features/cube.png";
+
 import styles from "../shared.module.scss";
 
 const FeatureCube = () => {
   return (
-    <div className={styles.featureContentContainer}>
-      <p>Coming soon.</p>
+    <div className={styles.featureFlexRow}>
+      <div className={styles.featureFlex3}>
+        <p>A 2D interactive Rubik&apos;s Cube map built with Processing.</p>
+        <p>Created for Production Resource Group (PRG), Las Vegas, for projection mapping on a 3D cube.</p>
+        <br />
+        <p>Play and check out the source code on OpenProcessing</p>
+        <p>
+          Test it out unshuffled:{" "}
+          <a
+            className={styles.featureLink}
+            href="https://www.openprocessing.org/sketch/470700"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://www.openprocessing.org/sketch/470700
+          </a>
+        </p>
+        <p>
+          Start shuffled:{" "}
+          <a
+            className={styles.featureLink}
+            href="https://www.openprocessing.org/sketch/470724"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://www.openprocessing.org/sketch/470724
+          </a>
+        </p>
+      </div>
+      <a
+        href="https://www.openprocessing.org/sketch/470724"
+        target="_blank"
+        rel="noreferrer"
+        className={styles.featureFlex2}
+      >
+        <Image src={cubeImage} alt="Net of the Rubik's cube" />
+      </a>
     </div>
   );
 };
